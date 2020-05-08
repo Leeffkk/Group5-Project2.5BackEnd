f927182a97b4e4463f5a3ce71c3edda9a77b23ba
kimjin <jinente@udel.edu>
2020-05-08 13:37:10 -0400

    start

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Project3';
+  activeClass = 'activeClass';
+  activeClassR = 'activeClassR';
+  dark = true;
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'http://www.eecis.udel.edu/3100'
+};
